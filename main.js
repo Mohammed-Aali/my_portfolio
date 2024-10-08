001504934d9b674d15c7a7c543263f1d73b40845
dark7nights <alalimohammed258@gmail.com>
2024-10-08 06:55:10 +0300

    some modification to my website

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 const buttonOne = document.querySelector(".button-one");
 const navMenu = document.querySelector(".nav-list");
 const navItems = [...navMenu.querySelectorAll("li")];
-console.log(navItems)
+const body = document.querySelector("body")
 
 buttonOne.addEventListener("click", (e) => {
     const isOpened = buttonOne.getAttribute("aria-expanded");
     if (isOpened === "false") {
         buttonOne.setAttribute('aria-expanded', 'true');
-        navMenu.style.left = "0px"
+        navMenu.style.right = "0px"
     } else {
-        buttonOne.setAttribute('aria-expanded', 'false');
-        navMenu.style.left = "-250px"
+        closeMenu(buttonOne)
     }
 })
 
@@ -19,8 +18,19 @@ navMenu.addEventListener("click", (e) => {
 
    if (clickedItem) {
        navItems.forEach(item => item.classList.remove("active"));
-
        clickedItem.classList.add("active");
-   }
-    
+       closeMenu(buttonOne)
+    }   
 })
+
+body.addEventListener('click', (event) => {
+    let menuState = buttonOne.getAttribute("aria-expanded")
+    if (menuState === "true" && !buttonOne.contains(event.target) && !navMenu.contains(event.target)) {
+        closeMenu(buttonOne); 
+    }
+})
+
+function closeMenu(menu) {
+    menu.setAttribute('aria-expanded', 'false');
+    navMenu.style.right = "-250px"  
+}
